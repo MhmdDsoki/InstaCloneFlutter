4d6f9a5de428e61a734096092ce96e49112fccab
m.sayed <abualgaitad@gmail.com>
2019-05-09 15:34:23 +0200

    -add search screen

--- lib/search/insta_list_search.dart
+++ lib/search/insta_list_search.dart
@@ -16,123 +16,32 @@ class InstaListSearch extends StatelessWidget {
               height: deviceSize.height * 0.08,
             )
           : index == 1
-              ? Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    Padding(
-                      padding:
-                          const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 16.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Row(
-                            children: <Widget>[
-                              new Container(
-                                height: 40.0,
-                                width: 40.0,
-                                decoration: new BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  image: new DecorationImage(
-                                      fit: BoxFit.fill,
-                                      image: new NetworkImage(
-                                          "https://pbs.twimg.com/profile_images/916384996092448768/PF1TSFOE_400x400.jpg")),
-                                ),
-                              ),
-                              new SizedBox(
-                                width: 10.0,
-                              ),
-                              new Text(
-                                "imthpk",
-                                style: TextStyle(fontWeight: FontWeight.bold),
-                              )
-                            ],
-                          ),
-                          new IconButton(
-                            icon: Icon(Icons.more_vert),
-                            onPressed: null,
-                          )
-                        ],
-                      ),
-                    ),
-                    Flexible(
-                      fit: FlexFit.loose,
-                      child: new Image.network(
-                        "https://images.pexels.com/photos/672657/pexels-photo-672657.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          new Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              new Icon(
-                                Icons.favorite,
-                              ),
-                              new SizedBox(
-                                width: 16.0,
-                              ),
-                              new Icon(
-                                Icons.comment,
-                              ),
-                              new SizedBox(
-                                width: 16.0,
-                              ),
-                              new Icon(Icons.send),
-                            ],
-                          ),
-                          new Icon(Icons.bookmark)
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                      child: Text(
-                        "Liked by pawankumar, pk and 528,331 others",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(16.0, 16.0, 0.0, 8.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: <Widget>[
-                          new Container(
-                            height: 40.0,
-                            width: 40.0,
-                            decoration: new BoxDecoration(
-                              shape: BoxShape.circle,
-                              image: new DecorationImage(
-                                  fit: BoxFit.fill,
-                                  image: new NetworkImage(
-                                      "https://pbs.twimg.com/profile_images/916384996092448768/PF1TSFOE_400x400.jpg")),
+              ? new SizedBox(
+                  height: deviceSize.height * 0.40,
+                  child: Row(
+                    children: <Widget>[
+                      Flexible(
+                        fit: FlexFit.tight,
+                        child: Column(
+                          children: <Widget>[
+                            new Expanded(
+                              child: Image.network(
+                                  "https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_960_720.jpg"),
                             ),
-                          ),
-                          new SizedBox(
-                            width: 10.0,
-                          ),
-                          Expanded(
-                            child: new TextField(
-                              decoration: new InputDecoration(
-                                border: InputBorder.none,
-                                hintText: "Add a comment...",
-                              ),
+                            new Expanded(
+                              child: Image.network(
+                                  "https://images4.alphacoders.com/899/899389.jpg"),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                      child: Text("1 Day Ago",
-                          style: TextStyle(color: Colors.grey)),
-                    )
-                  ],
+                      Flexible(
+                        fit: FlexFit.tight,
+                        child: Image.network(
+                            "https://isdb.pw/upload/250888886/1520870040902105012.jpg"),
+                      ),
+                    ],
+                  ),
                 )
               : new SizedBox(
                   child: new FirstScreen(),
